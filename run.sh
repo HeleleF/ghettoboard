8c14fe1f852f578dcbe832c53f874bcb0b4d7f44
Your Name <you@example.com>
2023-11-08 16:55:51 +0000

    Add script

---
+++ run.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo $HOME
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# pnpm
+export PNPM_HOME="/home/helene/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+cd /home/helene/ghettoboard
+pnpm start
